d1d6796b879cac9a2802b21f9837e2e8a92e1826
Sere0708 <103060203+Sere0708@users.noreply.github.com>
2022-09-21 22:56:36 +0300

    Java5

---
+++ Laba.java
@@ -0,0 +1,16 @@
+/*import java.util.Scanner;
+
+public class Laba{
+    
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+            System.out.println("Введите строку текста");
+            String stroka = scan.nextLine();
+            stroka = stroka.replaceAll("[^a-zA-Zа-яА-Я]", " ");
+            System.out.println(stroka);
+            System.out.println("Количество слов: "+stroka.split("\\s+").length);                 
+    }   
+}
+*/
+
